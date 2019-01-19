6c469d8e7c8a7f39bd8c99f7ca70cdfe7e1e7c5b
johannes-hoetter <johannes.hoetter@smail.inf.h-brs.de>
2019-01-19 11:18:45 +0100

    Collision moves player back to start

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Enemies our player must avoid
 class Enemy {
 
-    constructor(x, y) {
+    constructor(x, y, speed) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
 
@@ -10,6 +10,7 @@ class Enemy {
         this.sprite = 'images/enemy-bug.png';
         this.x = x;
         this.y = y;
+        this.speed = speed;
     }
 
     // Update the enemy's position, required method for game
@@ -18,6 +19,18 @@ class Enemy {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
+        const mvmt = this.speed * dt;
+        this.x += mvmt;
+        if (this.x > 505) {
+            //when the enemy moves out of the screen, reset its location
+            //slightly left to the scren (so it moves into the screen again)
+            this.x = -100;
+        }
+
+        if (this.x >= player.x -35 && this.x <= player.x + 35 &&
+            this.y == player.y) {
+            player.init();
+        }
     }
 
     // Draw the enemy on the screen, required method for game
@@ -32,8 +45,10 @@ class Enemy {
 
 class Player {
 
-    constructor(x, y) {
+    constructor(x = 202, y = 390) {
         this.sprite = 'images/char-boy.png';
+        this.beginX = x
+        this.beginY = y;
         this.x = x;
         this.y = y;
     }
@@ -47,9 +62,9 @@ class Player {
     }
 
     handleInput(keyPress) {
-        //hoch: y Position um 75 reduzieren
+        //hoch: y Position um 85 reduzieren
         //links: x Position um 101 reduzieren
-        //runter: y Position um 75 erhöhen
+        //runter: y Position um 85 erhöhen
         //rechts: x Position um 101 erhöhen
         switch (keyPress) {
             case 'up':
@@ -72,27 +87,32 @@ class Player {
                     this.x += 101;
                 }
         }
-        console.log(this.x, this.y);
+        //console.log(this.x, this.y);
+    }
+
+    init() {
+        this.x = this.beginX;
+        this.y = this.beginY;
     }
 }
 
 
 const row = new Map();
-row.set('1st', 390);
-row.set('2nd', 305);
-row.set('3rd', 220);
-row.set('4th', 135);
-row.set('5th', 50);
-let y1 = row.get('1st');
-let y2 = row.get('2nd');
-let y3 = row.get('3rd');
-let y4 = row.get('4th');
-let y5 = row.get('5th');
+row.set('1', 390); //top row (one row before finish line)
+row.set('2', 305);
+row.set('3', 220);
+row.set('4', 135);
+row.set('5', 50);  //bottom row (start row)
+let y1 = row.get('1');
+let y2 = row.get('2');
+let y3 = row.get('3');
+let y4 = row.get('4');
+let y5 = row.get('5');
 
-const player = new Player(202, 390); //start in the middle bottom (canvas has size of 505 x 606)
+const player = new Player(); //start in the middle bottom (canvas has size of 505 x 606)
 
 //testweise
-let allEnemies = [new Enemy(0, y1), new Enemy(0, y2), new Enemy(0, y3), new Enemy(0, y4), new Enemy(0, y5)];
+let allEnemies = [new Enemy(0, y1, 100), new Enemy(0, y2, 50), new Enemy(0, y3, 200)];
 
 
 // This listens for key presses and sends the keys to your
