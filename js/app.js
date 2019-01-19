52f7b5fbdc7251370511d4b1ce59fe81cb585ed9
johannes-hoetter <johannes.hoetter@smail.inf.h-brs.de>
2019-01-19 10:45:56 +0100

    Placed Figures testwise
    get a feeling for x/y coordinates on canvas

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 // Enemies our player must avoid
 class Enemy {
 
-    constructor() {
+    constructor(x, y) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
 
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
     }
 
     // Update the enemy's position, required method for game
@@ -30,8 +32,10 @@ class Enemy {
 
 class Player {
 
-    constructor() {
+    constructor(x, y) {
         this.sprite = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
     }
 
     update() {
@@ -39,16 +43,34 @@ class Player {
     }
 
     render() {
-
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
     handleInput() {
-
+        //hoch: y Position um 75 reduzieren
+        //links: x Position um 101 reduzieren
+        //runter: y Position um 75 erhöhen
+        //rechts: x Position um 101 erhöhen
     }
 }
 
-const player = Player();
-let allEnemies = [];
+
+const row = new Map();
+row.set('1st', 390);
+row.set('2nd', 305);
+row.set('3rd', 220);
+row.set('4th', 135);
+row.set('5th', 50);
+let y1 = row.get('1st');
+let y2 = row.get('2nd');
+let y3 = row.get('3rd');
+let y4 = row.get('4th');
+let y5 = row.get('5th');
+
+const player = new Player(202, 390); //start in the middle bottom (canvas has size of 505 x 606)
+
+//testweise
+let allEnemies = [new Enemy(0, y1), new Enemy(0, y2), new Enemy(0, y3), new Enemy(0, y4), new Enemy(0, y5)];
 
 
 // This listens for key presses and sends the keys to your
